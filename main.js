cc8514760db6a25f51a84817238044b05d79b74e
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2022-11-14 18:00:21 +0100

    hid the mail adress

---
+++ main.js
@@ -0,0 +1,3 @@
+var encEmail = "ZC5hdGtpbnM2QGljbG91ZC5jb20=";
+const form = document.getElementById("testthis");
+form.setAttribute("href", "mailto:".testthis(atob(encEmail))"?subject=Kontaktaufnahme");
