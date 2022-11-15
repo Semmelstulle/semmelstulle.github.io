44d21e9a1ef9b0d1ee0f814f810f5c2489489f88
SemmelStulle <74740271+SemmelStulle@users.noreply.github.com>
2022-11-15 17:03:27 +0100

    can hide footer now

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const footerToHide = document.getElementById("footer");
+    const xbtn = document.getElementById("footerx");
+    xbtn.onclick = function () {
+        footerToHide.style.display = "none";
+    }
+});
